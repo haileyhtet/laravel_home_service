10f6104c5abeec490a39f47c50182db5db9d1cf5
haileyhtet <haileyhtet@gmail.com>
2022-02-20 17:56:20 -1200

    6 Create Service Categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\ServiceCategory;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            ServiceCategorySeeder::class
+
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Customer\CustomerDashboardComponent;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ServiceCategoriesComponent;
 use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
+use App\Models\ServiceCategory;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/',HomeComponent::class)->name('home');
-
+Route::get('/service-categories',ServiceCategoriesComponent::class)->name('home.service-categories');
 //For Customer
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/customer/dashboard',CustomerDashboardComponent::class)->name('customer.dashboard');
